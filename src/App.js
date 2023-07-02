352154139fdd1786af7779b2460ee0950bfc02df
nivin77789 <nivin77789@gmail.com>
2023-07-02 22:05:02 +0530

    title

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="nav">Revolve</div>
-      <div className="main">Main</div>
+      <div className="taskbar">revolve</div>
+      <div className="sub">
+        <div className="nav">Dashboard</div>
+        <div className="main">Main</div>
+      </div>
     </div>
   );
 }
