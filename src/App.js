a4f2a3a98635d584c9ae9771e3287f2aaf6b1279
nivin77789 <nivin77789@gmail.com>
2023-07-03 23:31:22 +0530

    dash

--- src/App.js
+++ src/App.js
@@ -102,7 +102,56 @@ function App() {
         </div>
       </div>
       <div className="sub">
-        <div className="nav">Dashboard</div>
+        <div className="nav">
+          <div className="dash bord">
+            <span class="material-symbols-outlined">dashboard</span>
+            <h1>Dashboard</h1>
+          </div>
+          <div className="dash lead">
+            <span class="material-symbols-outlined">leaderboard</span>
+            <h1>Leaderboard</h1>
+          </div>
+          <div className="dash fore">
+            <span class="material-symbols-outlined">storm</span>
+            <h1>Forecast</h1>
+          </div>
+          <div className="dash top">
+            <span class="material-symbols-outlined">local_mall</span>
+            <h1>Top products</h1>
+          </div>
+          <div className="dash sales">
+            <span class="material-symbols-outlined">finance</span>
+            <h1>Sales Report</h1>
+          </div>
+          <div className="dash invent">
+            <span class="material-symbols-outlined">inventory</span>
+            <h1>Inventory</h1>
+          </div>
+          <div className="dash order">
+            <span class="material-symbols-outlined">shopping_cart</span>
+            <h1>Orders</h1>
+          </div>
+          <div className="dash cust">
+            <span class="material-symbols-outlined">lab_profile</span>
+            <h1>Customer Report</h1>
+          </div>
+          <div className="dash pay">
+            <span class="material-symbols-outlined">credit_card</span>
+            <h1>Payment</h1>
+          </div>
+          <div className="dash dark">
+            <span class="material-symbols-outlined">nightlight</span>
+            <h1>Dark mode</h1>
+          </div>
+          <div className="dash set">
+            <span class="material-symbols-outlined">settings</span>
+            <h1>settings</h1>
+          </div>
+          <div className="sign">
+            <span class="material-symbols-outlined">logout</span>
+            <h1>sign out</h1>
+          </div>
+        </div>
         <div className="main">Main</div>
       </div>
     </div>
