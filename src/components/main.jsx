fbe5201a5f6a781db64badc0b740ece82208c168
nivin77789 <nivin77789@gmail.com>
2023-07-04 19:21:43 +0530

    maind

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,17 +7,42 @@ function Main() {
       <div className="tope">
         <div className="sale">
           <div className="sale-top">
-            <div className="sale-left">hi</div>
-            <div className="sale-right">by</div>
+            <div className="sale-left">
+              <h1>Dashboard overview</h1>
+            </div>
+            <div className="sale-right">
+              <h1>Add your Data</h1>
+            </div>
           </div>
-          <div className="sale-down">hello</div>
+          <div className="sale-down">
+            <h1>Today's Sales</h1>
+          </div>
         </div>
-        <div className="insight">insight</div>
+        <div className="insight">
+          <h1>Customer Insights</h1>
+        </div>
       </div>
       <div className="mide">
-        <div className="mid-left">1</div>
-        <div className="mid-center">2</div>
-        <div className="mid-right">3</div>
+        <div className="mid-left">
+          <h1>Total Revenue</h1>
+        </div>
+        <div className="mid-center">
+          <h1>Time Analysis</h1>
+        </div>
+        <div className="mid-right">
+          <h1>Target vs Reality</h1>
+        </div>
+      </div>
+      <div className="bot">
+        <div className="bot-left">
+          <h1>Top Products</h1>
+        </div>
+        <div className="bot-center">
+          <h1>Sales by Region</h1>
+        </div>
+        <div className="bot-right">
+          <h1></h1>
+        </div>
       </div>
     </div>
   );
