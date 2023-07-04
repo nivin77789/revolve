d5e18151b5badb9fe304d1a1d1d1de264e874870
nivin77789 <nivin77789@gmail.com>
2023-07-04 18:28:17 +0530

    main up

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "../App.css";
 
 function Main() {
-  return <div>main field</div>;
+  return (
+    <div className="main">
+      <div className="tope">
+        <div className="sale">
+          <div className="sale-top">
+            <div className="sale-left">hi</div>
+            <div className="sale-right">by</div>
+          </div>
+        </div>
+        <div className="insight">insight</div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
