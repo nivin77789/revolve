4ca4ab272b49a95ee26990d8b4179ebc86a0cd0d
nivin77789 <nivin77789@gmail.com>
2023-07-04 18:51:19 +0530

    main d

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -10,8 +10,14 @@ function Main() {
             <div className="sale-left">hi</div>
             <div className="sale-right">by</div>
           </div>
+          <div className="sale-down">hello</div>
         </div>
         <div className="insight">insight</div>
+      </div>
+      <div className="mide">
+        <div className="mid-left">1</div>
+        <div className="mid-center">2</div>
+        <div className="mid-right">3</div>
       </div>
     </div>
   );
