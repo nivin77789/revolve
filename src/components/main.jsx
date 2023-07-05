09079477a513cdebd9527cfe9c22e92538fc63d8
nivin77789 <nivin77789@gmail.com>
2023-07-05 16:35:25 +0530

    box

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -18,19 +18,55 @@ function Main() {
               <h1>Add your Data</h1>
               <p>(.csv file)</p>
               <div className="imp-button">
-                <span class="material-symbols-outlined">system_update_alt</span>
+                <span class="material-symbols-outlined">download</span>
                 <p>Import</p>
               </div>
             </div>
           </div>
           <div className="sale-down">
-            <div>
-              <h1>Today's Sales</h1>
-              <p>Sales Summery</p>
+            <div className="sale-d-top">
+              <div>
+                <h1>Today's Sales</h1>
+                <p>Sales Summery</p>
+              </div>
+              <div className="exp-button">
+                <span class="material-symbols-outlined">upload</span>
+                <p>Export</p>
+              </div>
             </div>
-            <div className="exp-button">
-              <span class="material-symbols-outlined">ios_share</span>
-              <p>Export</p>
+            <div className="sale-d-bot">
+              <div className="box box1">
+                <div className="round1">
+                  <span class="material-symbols-outlined">lab_profile</span>{" "}
+                </div>
+                <h4>$1k</h4>
+                <h2>Total Sales</h2>
+                <h3>+8% from yesterday</h3>
+              </div>
+              <div className="box box2">
+                <div className="round2">
+                  <span class="material-symbols-outlined">lab_profile</span>{" "}
+                </div>
+                <h4>$1k</h4>
+                <h2>Total Sales</h2>
+                <h3>+8% from yesterday</h3>
+              </div>
+              <div className="box box3">
+                <div className="round3">
+                  <span class="material-symbols-outlined">lab_profile</span>{" "}
+                </div>
+                <h4>$1k</h4>
+                <h2>Total Sales</h2>
+                <h3>+8% from yesterday</h3>
+              </div>
+              <div className="box box4">
+                <div className="round4">
+                  <span class="material-symbols-outlined">lab_profile</span>{" "}
+                </div>
+                <h4>$1k</h4>
+                <h2>Total Sales</h2>
+                <h3>+8% from yesterday</h3>
+              </div>
             </div>
           </div>
         </div>
