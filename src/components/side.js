1fd6ba82410419e4db64e301e26bc778f81e145e
nivin77789 <nivin77789@gmail.com>
2023-07-07 11:57:48 +0530

    task but

--- src/components/side.js
+++ src/components/side.js
@@ -12,46 +12,46 @@ function Side() {
         <span class="material-symbols-outlined">leaderboard</span>
         <h1>Leaderboard</h1>
       </button>
-      <div className="dash fore">
+      <button className="dash fore">
         <span class="material-symbols-outlined">storm</span>
         <h1>Forecast</h1>
-      </div>
-      <div className="dash top">
+      </button>
+      <button className="dash top">
         <span class="material-symbols-outlined">local_mall</span>
         <h1>Top products</h1>
-      </div>
-      <div className="dash sales">
+      </button>
+      <button className="dash sales">
         <span class="material-symbols-outlined">finance</span>
         <h1>Sales Report</h1>
-      </div>
-      <div className="dash invent">
+      </button>
+      <button className="dash invent">
         <span class="material-symbols-outlined">inventory</span>
         <h1>Inventory</h1>
-      </div>
-      <div className="dash order">
+      </button>
+      <button className="dash order">
         <span class="material-symbols-outlined">shopping_cart</span>
         <h1>Orders</h1>
-      </div>
-      <div className="dash cust">
+      </button>
+      <button className="dash cust">
         <span class="material-symbols-outlined">lab_profile</span>
         <h1>Customer Report</h1>
-      </div>
-      <div className="dash pay">
+      </button>
+      <button className="dash pay">
         <span class="material-symbols-outlined">credit_card</span>
         <h1>Payment</h1>
-      </div>
-      <div className="dash dark">
+      </button>
+      <button className="dash dark">
         <span class="material-symbols-outlined">nightlight</span>
         <h1>Dark mode</h1>
-      </div>
-      <div className="dash set">
+      </button>
+      <button className="dash set">
         <span class="material-symbols-outlined">settings</span>
         <h1>settings</h1>
-      </div>
-      <div className="sign">
+      </button>
+      <button className="sign">
         <span class="material-symbols-outlined">logout</span>
         <h1>sign out</h1>
-      </div>
+      </button>
     </div>
   );
 }
