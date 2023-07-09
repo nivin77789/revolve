269ebccf4e6fbf5ed2881f318a38f12d95e6aa72
nivin77789 <nivin77789@gmail.com>
2023-07-09 11:15:56 +0530

    username updat

--- src/components/taskbar.jsx
+++ src/components/taskbar.jsx
@@ -43,7 +43,7 @@ function Taskbar() {
             data-imagecss="flag ae"
             data-title="United Arab Emirates"
           >
-            United Arab Emirates
+            United Arab Emirate
           </option>
           <option
             value="af"
