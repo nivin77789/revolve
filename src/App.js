400588b0a3b8f4640afa964ea361772ab6b14d75
nivin77789 <nivin77789@gmail.com>
2023-07-20 17:22:50 +0530

    cvbn

--- src/App.js
+++ src/App.js
@@ -5,14 +5,7 @@ import Taskbar from "./components/taskbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Taskbar />
-      <div className="sub">
-        <Side />
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
