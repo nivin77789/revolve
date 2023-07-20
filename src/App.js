d43d8b531b8d8dcd68317f808fe5c813bfcbb455
nivin77789 <nivin77789@gmail.com>
2023-07-20 17:19:27 +0530

    removed menu

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Side />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Main />}></Route>
+            <Route path="/hi" element={<Main />}></Route>
           </Routes>
         </BrowserRouter>
       </div>
